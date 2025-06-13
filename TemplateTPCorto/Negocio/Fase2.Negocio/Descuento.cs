2cfdacb043c81c15f4d9e731fad13fbc97690442
aleten12 <alejandramtenor@gmail.com>
2025-06-13 19:26:53 -0300

    Update Descuento.cs

--- TemplateTPCorto/Negocio/Fase2.Negocio/Descuento.cs
+++ TemplateTPCorto/Negocio/Fase2.Negocio/Descuento.cs
@@ -16,8 +16,6 @@ namespace Negocio.Fase2.Negocio
             out bool aplicaDescuento)
         {
             subtotal = 0;
-            decimal subtotalElectroHogar = 0;
-            const string categoriaElectroHogar = "Electro Hogar";
 
             foreach (var item in carrito)
             {
@@ -25,19 +23,12 @@ namespace Negocio.Fase2.Negocio
                 int cantidad = item.Item2;
                 decimal totalProducto = producto.Precio * cantidad;
                 subtotal += totalProducto;
-
-                var categoria = categorias.FirstOrDefault(c => c.Id == producto.IdCategoria.ToString());
-
-                if (categoria != null && categoria.Detalle == categoriaElectroHogar)
-                {
-                    subtotalElectroHogar += totalProducto;
-                }
             }
 
-            aplicaDescuento = subtotalElectroHogar > 1000000;
+            aplicaDescuento = subtotal > 1000000;
 
-            decimal descuentoElectroHogar = aplicaDescuento ? subtotalElectroHogar * 0.15m : 0;
-            return subtotal - descuentoElectroHogar;
+            decimal descuento = aplicaDescuento ? subtotal * 0.15m : 0;
+            return subtotal - descuento;
         }
 
     }
